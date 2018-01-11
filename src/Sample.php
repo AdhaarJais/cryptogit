935636c59eafa96eb5d986a303f90eb9421ad9f0
sharma21sarita <enggphp.01@gmail.com>
2018-01-11 13:51:03 +0530

    Add sample file

---
+++ src/Sample.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class Sample
+{
+    public function sample()
+    {
+        return 'Hello!';
+    }
+}
